4ebf91859c4accb024bc4901e52914eb316dd3b6
KatlegoMothoa <kmothoa50@gmail.com>
2025-09-08 22:35:12 +0200

    new Git java file

---
+++ Git.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Git {
+    public static void main(String [] args){
+        Scanner kb = new Scanner(System.in);
+
+        String username = "";
+
+        System.out.println("Enter your username: ");
+        username = kb.nextLine();
+
+        System.out.println("My name is "+username+" and i'm stalking Katlego Mothoa. I don't wanna lie, this guy is destined for greatness.");
+
+    }
+}
