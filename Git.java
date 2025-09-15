568cc4ef27d7994e22277e749688c7dd49a33fff
KatlegoMothoa <kmothoa50@gmail.com>
2025-09-15 15:18:41 +0200

    bank status

--- Git.java
+++ Git.java
@@ -6,6 +6,8 @@ public class Git {
 
         String username = "";
         String surname = "";
+        String statement = "";
+        double money = 0.0;
 
         System.out.println("Enter your username: ");
         username = kb.nextLine();
@@ -13,7 +15,19 @@ public class Git {
         System.out.println("Enter your Surname: ");
         surname = kb.nextLine();
 
-        System.out.println("I am "+username+" "+surname+" and i'm stalking Katlego Mothoa. I don't wanna lie, this guy is destined for greatness.");
+        System.out.print("Enter the amount of money you got: R");
+        money = kb.nextDouble();
 
+        if(money < 1000){
+            statement = "I am broke";
+        } else if (money >= 1000 && money < 5000) {
+            statement = "I am doing okay";
+        } else if (money >= 5000 && money < 10000) {
+            statement = "I am rich";
+        } else {
+            statement = "I am very rich";
+        }
+        System.out.println("I am "+username+" "+surname+" and i'm stalking Katlego Mothoa. I don't wanna lie, this guy is destined for greatness.");
+        System.out.println("Also, based on my bank balance, "+statement);
     }
 }
